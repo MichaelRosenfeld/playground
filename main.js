37b2e7332ea107d4e8368ead8dbd03d1051d25fe
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-10-11 10:23:37 +0200

    Exercise 4 and 5 done

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+// Variables
+
 console.log("Starting JS");
 
 const myName = "Michael";
@@ -10,3 +13,19 @@ const ignasiAge = 32;
 const ageDiff = myAge - ignasiAge;
 
 console.log(ageDiff);
+
+// Conditionals
+
+if (myAge > 21) {
+    console.log("You are older than 21");
+} else {
+    console.log("You are younger than 21");
+}
+
+if (myAge > ignasiAge) {
+    console.log("You are older than Ignasi");
+} else if (myAge === ignasiAge) {
+    console.log("You are the same age as Ignasi");
+} else {
+    console.log("You are younger than Ignasi");
+}
