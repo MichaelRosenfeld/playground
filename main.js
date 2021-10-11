3a9b4f5b124cc43f015bfda72f6d0e7e92ef80f7
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-10-11 10:18:28 +0200

    First 3 Exercises done.

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Starting JS");
+
+const myName = "Michael";
+console.log(myName);
+
+const myAge = 30;
+console.log(myAge);
+
+const ignasiAge = 32;
+const ageDiff = myAge - ignasiAge;
+
+console.log(ageDiff);
